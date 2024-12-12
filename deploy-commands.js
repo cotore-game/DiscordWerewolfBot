f62fce59ef857c28687003cef92f113781fbf151
コトレ <102813037+cotore-game@users.noreply.github.com>
2024-12-13 01:06:31 +0900

    Add Command
    実験用のコマンドを追加。

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,22 @@
-// discord.js v14では、下記のようにRESTとRoutesはdiscord.jsパッケージから直接インポートできます
+// discord.js v14では、下のようにRESTとRoutesはdiscord.jsパッケージから直接インポート可能
 const { REST, Routes } = require('discord.js');
-
-// hey.jsのmodule.exportsを呼び出します。
-const heyFile = require('./commands/hey.js');
-
-// 環境変数としてapplicationId, guildId, tokenの3つが必要です
+// 環境変数としてapplicationId, guildId, tokenの3つが必要
 const { applicationId, guildId, token } = require('./config.json');
+// hey.jsのmodule.exportsを呼び出す
+const { commands } = require('./commands/hey.js');
 
-// 登録コマンドを呼び出してリスト形式で登録
-const commands = [heyFile.data.toJSON()];
+// すべてのコマンドをリスト形式で登録
+const commandData = commands.map(command => command.data.toJSON());
 
 // DiscordのAPIには現在最新のversion10を指定
 const rest = new REST({ version: '10' }).setToken(token);
 
-// Discordサーバーにコマンドを登録
 (async () => {
     try {
         await rest.put(
-			Routes.applicationGuildCommands(applicationId, guildId),
-			{ body: commands },
-		);
+            Routes.applicationGuildCommands(applicationId, guildId),
+            { body: commandData },
+        );
         console.log('サーバー固有のコマンドが登録されました！');
     } catch (error) {
         console.error('コマンドの登録中にエラーが発生しました:', error);
