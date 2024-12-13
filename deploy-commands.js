2205ae321f55a7ad349b6a8c33d124096f216a2d
コトレ <102813037+cotore-game@users.noreply.github.com>
2024-12-13 11:48:03 +0900

    コマンドの追加
    newgameとstartgame

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,11 +2,25 @@
 const { REST, Routes } = require('discord.js');
 // 環境変数としてapplicationId, guildId, tokenの3つが必要
 const { applicationId, guildId, token } = require('./config.json');
-// hey.jsのmodule.exportsを呼び出す
-const { commands } = require('./commands/fund.js');
+// jsのmodule.exportsを呼び出す
+//const { commands } = require('./commands/fund.js');
+
+// ディレクトリを読み込む為
+const { readdirSync } = require('fs');
 
 // すべてのコマンドをリスト形式で登録
-const commandData = commands.map(command => command.data.toJSON());
+//const commandData = commands.map(command => command.data.toJSON());
+
+// コマンドフォルダをスキャンしてすべてのコマンドを収集
+const commands = [];
+const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const commandModule = require(`./commands/${file}`);
+    if (Array.isArray(commandModule.commands)) {
+        commands.push(...commandModule.commands.map(cmd => cmd.data.toJSON()));
+    }
+}
 
 // DiscordのAPIには現在最新のversion10を指定
 const rest = new REST({ version: '10' }).setToken(token);
@@ -15,7 +29,7 @@ const rest = new REST({ version: '10' }).setToken(token);
     try {
         await rest.put(
             Routes.applicationGuildCommands(applicationId, guildId),
-            { body: commandData },
+            { body: commands },
         );
         console.log('サーバー固有のコマンドが登録されました！');
     } catch (error) {
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const { commands } = require('./commands/fund.js');
+//const { commands } = require('./commands/fund.js');
+// ディレクトリから読み込む
+const { readdirSync } = require('fs');
+
+// コマンドを保持するマップ
+const commands = new Map();
+const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// 各コマンドファイルからコマンドをロード
+for (const file of commandFiles) {
+    const commandModule = require(`./commands/${file}`);
+    if (Array.isArray(commandModule.commands)) {
+        for (const cmd of commandModule.commands) {
+            commands.set(cmd.data.name, cmd);
+        }
+    }
+}
 
 // クライアントインスタンスを作成
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -8,16 +24,18 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // クライアントオブジェクトが準備OKとなったとき一度だけ実行される(と思う)
 client.once(Events.ClientReady, c => {
     console.log(`準備OKです! ${c.user.tag}がログインします。`);
+    console.log('Your Discord.js Version is ' + require('discord.js').version);
 });
 
 // スラッシュコマンドに応答するためのイベントリスナー
 client.on(Events.InteractionCreate, async interaction => {
 
-    // ここでスラッシュがついているかどうかを判断してreturn
+    // ここでスラッシュがついているかどうかを判断してreturn。 StartWithでも挙動はおなじ？だが、メソッド用意されてるなら使おう精神。
     if (!interaction.isChatInputCommand()) return;
 
     // 実行されたコマンドを探索
-    const command = commands.find(cmd => cmd.data.name === interaction.commandName);
+    //const command = commands.find(cmd => cmd.data.name === interaction.commandName);
+    const command = commands.get(interaction.commandName);
 
     if (!command) {
         console.error(`${interaction.commandName}というコマンドには対応していません。`);
